aa8032daad8416fc50ec7b95dbacaa35dee91d68
bruno <bruno..opermanis@gmail.com>
2022-02-16 22:28:24 +0200

    working on daemon

--- ai_rest_api.py
+++ ai_rest_api.py
@@ -6,16 +6,9 @@ from Translator import Translator, Flattener
 from config import repo_dir, log_dir
 import logging
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--ip', type=str, default="localhost")
-parser.add_argument('--port', type=int, default="2222")
-
-args = parser.parse_args()
 
 app = Flask(__name__)
 
-
-app.debug = True
 applogger = app.logger
 file_handler = logging.FileHandler(log_dir + "/app.log")
 file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
@@ -34,8 +27,10 @@ def predict():
         json = request.json
         # id_session = json['id_session']
         id_session = 0
+
         location = json["location"]
         id_bp = json['plan']['businessPlan_id']
+
         if id_session not in dict_sessions:
             dict_sessions[id_session] = MultiNetwork()
         guids = flattener(json)
@@ -45,12 +40,19 @@ def predict():
         guids = translator.back(translation)
         bp = flattener.back(guids)
         logging.info('returning num %s guids', len(guids))
+
         bp['location'] = location
         bp['plan']['businessPlan_id'] = id_bp
+
         return bp
     else:
         return 'Content-Type not supported!'
 
-
 if __name__ == "__name__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ip', type=str, default="localhost")
+    parser.add_argument('--port', type=int, default="2222")
+
+    args = parser.parse_args()
+
     app.run(args.ip, args.port)
--- ai_server.py
+++ ai_server.py
@@ -1,12 +1,131 @@
+import signal
+
 from gevent.pywsgi import WSGIServer
 from ai_rest_api import app
 import argparse
+import os
+from os.path import join
+from config import repo_dir
+import subprocess
+import sys
+import atexit
+from time import time, sleep
+import logging
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--ip', type=str, default="localhost")
-parser.add_argument('--port', type=int, default=2222)
 
-args = parser.parse_args()
+class Daemon:
+    def __init__(self, args=None):
+        self.pid_path = join(repo_dir, "pid.txt")
+        self.args = args
 
-http_server = WSGIServer((args.ip, args.port), app)
-http_server.serve_forever()
+    def daemonize(self):
+        pid = os.fork()
+        try:
+            if pid > 0:
+                sys.exit(0)
+        except OSError as err:
+            sys.stderr.write(f"fork #1 failed {err}\n")
+            sys.exit(1)
+
+        os.chdir("/")
+        os.setsid()
+        os.umask(0)
+
+        try:
+            pid = os.fork()
+            if pid > 0:
+                sys.exit(0)
+        except OSError as err:
+            sys.stderr.write(f"fork #2 failed {err}\n")
+            sys.exit(1)
+
+        sys.stdout.flush()
+        sys.stderr.flush()
+
+        si = open(os.devnull, 'r')
+        so = open(os.devnull, 'a+')
+        se = open(os.devnull, 'a+')
+
+        os.dup2(si.fileno(), sys.stdin.fileno())
+        os.dup2(so.fileno(), sys.stdout.fileno())
+        os.dup2(se.fileno(), sys.stderr.fileno())
+
+        atexit.register(self.delete_pid)
+        pid = str(os.getpid())
+        with open(self.pid_path, "w+") as conn:
+            conn.write(pid + '\n')
+
+    def delete_pid(self):
+        os.remove(self.pid_path)
+
+    def start(self):
+        try:
+            with open(self.pid_path, 'r') as conn:
+                pid = int(conn.read().strip())
+        except IOError:
+            pid = None
+
+        if pid:
+            sys.stderr.write(f"pidfile {self.pid_path} allready exist. Checking if daemon already up \n")
+
+            try:
+                os.kill(pid, 0)
+                sys.stderr.write(f"process {pid} allready up, stop before running again\n")
+                sys.exit(1)
+            except OSError:
+                self.delete_pid()
+
+        self.daemonize()
+        self.launch_server()
+
+    def launch_server(self):
+        logging.info("Starting WSGI server")
+        print(111111111111111)
+        # http_server = WSGIServer((args.ip, args.port), app)
+        # http_server.serve_forever()
+
+    def stop(self):
+        try:
+            with open(self.pid_path, "r") as conn:
+                pid = int(conn.read().strip())
+        except IOError:
+            pid = None
+
+        if not pid:
+            sys.stderr.write(f"pidfile {self.pid_path} doesn't exist, daemon not running?")
+            return
+
+        try:
+            while True:
+                os.kill(pid, signal.SIGTERM)
+                sleep(0.1)
+        except OSError as err:
+            error = str(err.args)
+            if error.find("No such process") > 0:
+                if os.path.exists(self.pid_path):
+                    os.remove(self.pid_path)
+            else:
+                logging.error("Daemon exception")
+                sys.exit()
+
+    def restart(self):
+        self.stop()
+        self.start()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('command', type=str)  # possible values start/stop/restart
+    parser.add_argument('--ip', type=str, default="localhost")
+    parser.add_argument('--port', type=int, default=2222)
+    args = parser.parse_args()
+
+    daemon = Daemon()
+    if args.command == "start":
+        daemon.start()
+    elif args.command == "stop":
+        daemon.stop()
+    elif args.command == "restart":
+        daemon.restart()
+    else:
+        print("wrong command")
