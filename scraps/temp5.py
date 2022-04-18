4f57d9fd42a3fdabb40066042ca4b342d724f939
bruno <bruno..opermanis@gmail.com>
2022-04-18 19:57:33 +0300

    defined dependencies in value_propositions.xdsl, made channels.xdsl with all dependencies, and set up channels, except in full_bp.json

---
+++ scraps/temp5.py
@@ -0,0 +1,13 @@
+import pickle
+from pprint import pprint
+from re import findall
+
+with open("../bp.pickle", "rb") as conn:
+    bp = pickle.load(conn)
+
+# print(findall('03f70344-c4f5-456a-bbfe-94dde489b31c', str(bp)))
+
+
+pprint(bp['plan']['channels'][1].keys())
+
+pprint(bp['plan']['channels'][1])
